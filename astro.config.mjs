04b6debfa5fd68225079387eda8fa426bcacb9dc
Guido Modarelli <guidomodarelli25@gmail.com>
2024-10-23 20:22:55 -0300

    Add SCSS modern compiler support in Vite config; enhance Astro setup for improved styles preprocessing and compatibility

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,14 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          api: 'modern-compiler' // or "modern"
+        }
+      }
+    },
+  },
   integrations: [tailwind(), react()]
 });
