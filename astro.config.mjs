92cce9190d1b4cc9845150cfc4e4075c250fcde2
Guido Modarelli <guidomodarelli25@gmail.com>
2024-10-23 21:29:36 -0300

    Remove React integration and related dependencies from the Astro config and package files for a lighter setup

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,6 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwind from '@astrojs/tailwind';
-
-import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
@@ -17,5 +14,5 @@ export default defineConfig({
       }
     },
   },
-  integrations: [tailwind(), react()]
+  integrations: [tailwind()]
 });
