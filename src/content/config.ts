350865197ed8f646656af9e42d7d2ead32497abe
Guido Modarelli <guidomodarelli25@gmail.com>
2024-11-03 18:54:12 -0300

    Add apps collection to config.ts, incorporating validation schema for application data alongside existing FAQs collection

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,12 +1,18 @@
 import { defineCollection } from 'astro:content';
-import { schemaFaq } from "./schema";
+import { schemaApp, schemaFaq } from "./schema";
 
 const faqsCollection = defineCollection({
   type: 'content',
   schema: schemaFaq,
 });
+
+const appsCollection = defineCollection({
+  type: 'content',
+  schema: schemaApp,
+});
 // 3. Export a single `collections` object to register your collection(s)
 //    This key should match your collection directory name in "src/content"
 export const collections = {
   faqs: faqsCollection,
+  apps: appsCollection,
 };
