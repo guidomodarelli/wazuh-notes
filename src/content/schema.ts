7c6b1010e776157254492713cd3f7c26fb90c7ec
Guido Modarelli <guidomodarelli25@gmail.com>
2024-11-03 18:54:06 -0300

    Add validation schema for app details in schema.ts, enhancing data integrity for application-related inputs

--- src/content/schema.ts
+++ src/content/schema.ts
@@ -1,4 +1,4 @@
-import { z } from "astro/zod";
+import { z } from 'astro/zod';
 
 export const schemaFaq = z.object({
   date: z.string({
@@ -19,4 +19,26 @@ export const schemaFaq = z.object({
   version: z.string({
     message: 'Version is required',
   }),
-})
+});
+
+export const schemaApp = z.object({
+  date: z.string({
+    message: 'Date is required',
+  }),
+  appname: z.string({
+    message: 'Appname is required',
+  }),
+  description: z.string({
+    message: 'Description is required',
+  }),
+  author: z.string({
+    message: 'Author is required',
+  }),
+  doclink: z
+    .string({
+      message: 'Doclink is required',
+    })
+    .url({
+      message: 'Doclink is not a valid URL',
+    }),
+});
