d370fb75a1a8d04a35b46732966deed69e62d124
Guido Modarelli <guidomodarelli25@gmail.com>
2024-11-03 14:50:26 -0300

    Refactor FAQ loading to leverage COLLECTION constant for improved clarity and maintainability in content management

--- src/content/get-adoc-paths.ts
+++ src/content/get-adoc-paths.ts
@@ -1,8 +1,8 @@
 import { glob } from 'glob';
 import path from 'path';
 
-export async function getAdocPaths() {
-  const adocRelativeFaqPaths = await glob('src/content/faqs/*.adoc');
+export async function getAdocPaths(dirpath: string) {
+  const adocRelativeFaqPaths = await glob(`src/content/${dirpath}/*.adoc`);
   const adocFaqFileNames = adocRelativeFaqPaths.map((relativeFaqPath) =>
     path.basename(relativeFaqPath, '.adoc'),
   );
