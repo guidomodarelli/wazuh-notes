d1a36d3d970a5e91cdf20084323b73dd4c052718
Guido Modarelli <guidomodarelli25@gmail.com>
2024-11-04 18:24:33 -0300

    Refactor adoc file retrieval: rename and consolidate methods for clearer intent in Home, app, and faq pages

--- src/content/get-adoc-paths.ts
+++ src/content/get-adoc-paths.ts
@@ -1,10 +1,8 @@
 import { glob } from 'glob';
 import path from 'path';
 
-export async function getAdocPaths(dirpath: string) {
+export async function extractAdocFilenamesFromDirectory(dirpath: string) {
   const adocRelativeFaqPaths = await glob(`src/content/${dirpath}/*.adoc`);
-  const adocFaqFileNames = adocRelativeFaqPaths.map((relativeFaqPath) =>
-    path.basename(relativeFaqPath, '.adoc'),
-  );
-  return adocFaqFileNames;
+
+  return adocRelativeFaqPaths.map((relativeFaqPath) => path.basename(relativeFaqPath, '.adoc'));
 }
