9135f7b0cf7eedb47664d005a82fbc6c0dc22304
Guido Modarelli <guidomodarelli25@gmail.com>
2024-11-06 09:58:32 -0300

    Refactor initialize-dropdown-listeners to improve type safety and functionality using FilterOption and filterOptions constants

--- src/scripts/initialize-dropdown-listeners.ts
+++ src/scripts/initialize-dropdown-listeners.ts
@@ -1,45 +1,71 @@
-import type { DropdownButton } from "../components/SearchWithDropdown.astro";
-import { hide, toggleVisibility } from "../ui/utils/show-hide";
+import { filterOptions } from "../constants/filter-options";
+import { COLLECTION } from "../content/constants";
+import { hide, toggleVisibility } from '../ui/utils/show-hide';
+import type { FilterOption } from "../types/filter-option";
 
 export const initializeDropdownListeners = () => {
   const dropdownButton = document.querySelector('#dropdown-button') as HTMLElement;
   const dropdown = document.querySelector('#dropdown') as HTMLElement;
   const dropdownButtons = document.querySelectorAll('[data-dropdown-button]') as NodeListOf<HTMLElement>;
 
-  const filterCardsByType = (dropdownButtonData: DropdownButton) => {
-    const cards = document.querySelectorAll('[data-card]') as NodeListOf<HTMLElement>;
+  dropdownButton.addEventListener('click', toggleVisibility.bind(null, dropdown));
+  dropdownButtons.forEach(handleDropdownButtonClick(dropdown, dropdownButton));
+  document.addEventListener('click', handleDocumentClick(dropdown, dropdownButton));
 
-    const shouldShowCard = (cardType: string | undefined, key: string): boolean => {
-      return key === 'all' || cardType === key;
-    };
+  initializeTypeFromQuery(dropdownButton);
+};
 
-    cards.forEach((card) => {
-      const cardType = card.dataset.cardType;
-      if (shouldShowCard(cardType, dropdownButtonData.key)) {
-        delete card.dataset.cardDontShowOnType;
-      } else {
-        card.dataset.cardDontShowOnType = '';
-      }
-    });
-  };
+const filterCardsByType = (key: string) => {
+  const cards = document.querySelectorAll('[data-card]') as NodeListOf<HTMLElement>;
 
-  const handleDropdownButtonClick = (button: HTMLElement) => {
+  cards.forEach((card) => {
+    const cardType = card.dataset.cardType;
+    if (shouldShowCard(cardType, key)) {
+      delete card.dataset.cardDontShowOnType;
+    } else {
+      card.dataset.cardDontShowOnType = '';
+    }
+  });
+};
+
+const shouldShowCard = (cardType: string | undefined, key: string): boolean => {
+  return key === 'all' || cardType === key;
+};
+
+const handleDropdownButtonClick =
+  (dropdown: HTMLElement, dropdownButton: HTMLElement) => (button: HTMLElement) => {
     button.addEventListener('click', () => {
-      const dropdownButtonData = JSON.parse(button.dataset.dropdownButton ?? '{}') as DropdownButton;
+      const dropdownButtonData = JSON.parse(button.dataset.dropdownButton ?? '{}') as FilterOption;
       // change the dropdown button label
       dropdownButton.querySelector('span')!.textContent = dropdownButtonData.label;
-      filterCardsByType(dropdownButtonData);
+      filterCardsByType(dropdownButtonData.key);
       hide(dropdown);
     });
   };
 
-  const handleDocumentClick = (event: MouseEvent) => {
-    if (!dropdown.contains(event.target as Node) && !dropdownButton.contains(event.target as Node)) {
-      hide(dropdown);
-    }
-  };
+const handleDocumentClick = (dropdown: HTMLElement, dropdownButton: HTMLElement) => (event: MouseEvent) => {
+  if (!dropdown.contains(event.target as Node) && !dropdownButton.contains(event.target as Node)) {
+    hide(dropdown);
+  }
+};
 
-  dropdownButton.addEventListener('click', toggleVisibility.bind(null, dropdown));
-  dropdownButtons.forEach(handleDropdownButtonClick);
-  document.addEventListener('click', handleDocumentClick);
+const isKnownType = (type: string) => {
+  return Object.values(COLLECTION).includes(type as COLLECTION);
+};
+
+const getFilterOption = (type: string) => {
+  const knownTypes = [COLLECTION.APPS, COLLECTION.FAQS, COLLECTION.VAGRANTS] as string[];
+  return filterOptions.find((option) => knownTypes.includes(option.key) && option.key === type);
+}
+
+const initializeTypeFromQuery = (dropdownButton: HTMLElement) => {
+  const type = new URLSearchParams(window.location.search).get('t');
+
+  // if the type is defined and is a known type
+  if (type && isKnownType(type)) {
+    // change the dropdown button label
+    console.log(getFilterOption(type));
+    dropdownButton.querySelector('span')!.textContent = getFilterOption(type)!.label;
+    filterCardsByType(type);
+  }
 };
