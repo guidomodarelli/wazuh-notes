5d7f36ac269faed1a55ca76a7a0999a6d8a2f1dd
Guido Modarelli <guidomodarelli25@gmail.com>
2024-11-06 09:14:35 -0300

    Refactor script imports in Home and Layout components for cleaner code organization and improved maintainability

---
+++ src/scripts/index.ts
@@ -0,0 +1,5 @@
+export * from './initialize-copy-buttons';
+export * from './initialize-dropdown-listeners';
+export * from './setup-copy-buttons-for-code-blocks';
+export * from './setup-search';
+export * from './transform-code-elements-for-copy-their-content';
