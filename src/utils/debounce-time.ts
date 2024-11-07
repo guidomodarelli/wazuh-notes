d69c016ed9fe7b323a279bc93cc181996fdd1087
Guido Modarelli <guidomodarelli25@gmail.com>
2024-11-06 22:40:26 -0300

    Remove unused debounceTime utility to streamline codebase and avoid redundancy after switching to lodash.debounce

--- src/utils/debounce-time.ts
+++
@@ -1,8 +0,0 @@
-export const debounceTime = (delay: number) => {
-  let timeoutId: ReturnType<typeof setTimeout> | undefined;
-
-  return (callback: () => void) => {
-    clearTimeout(timeoutId);
-    timeoutId = setTimeout(callback, delay);
-  };
-};
